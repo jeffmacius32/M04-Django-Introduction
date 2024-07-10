e92dbcbe4fe0f338c954c445ba42dcaca3550487
Jeff Macius <jeffmacius@Jeffs-iMac.local>
2024-07-09 23:10:27 -0400

    change html for website

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+# create post_list function, take a reques as a argument/parameter
+def post_list(request):
+    # return the request
+    return render(request, 'blog/post_list.html', {})
